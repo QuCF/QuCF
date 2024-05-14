77b7728d3b4d4f3de45a3a6660b8f86491d3a2c2
Ivan Novikau <novikau1@login20.chn.perlmutter.nersc.gov>
2024-05-13 19:06:36 -0700

    new

--- simulations/LCHS/DiffConv/D001_t01_kmax10/job-script.sh
+++ simulations/LCHS/DiffConv/D001_t01_kmax10/job-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -G 1
+##SBATCH -G 1
 ##SBATCH -C gpu&hbm80g
 #SBATCH -C gpu
 #SBATCH -n 1
@@ -9,10 +9,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=novikau1@llnl.gov
 #SBATCH --account=mp2_g
-##SBATCH --qos=shared
-#SBATCH --qos=regular
-#SBATCH -t 01:00:00
+#SBATCH --qos=shared
+##SBATCH --qos=regular
+#SBATCH -t 00:10:00
 #SBATCH -J CDE_t01_kmax10
 #
 ## To run
-/global/homes/n/novikau1/QuCF/QuCF/build_qucf/QuCF dc ./
+export SLURM_CPU_BIND="cores"
+srun /global/homes/n/novikau1/QuCF/QuCF/build_qucf/QuCF dc ./
