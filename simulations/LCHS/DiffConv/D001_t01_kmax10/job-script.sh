0c03232d7f23db27b22ef8959b01da7b37ca799a
Ivan Novikau <novikau1@login20.chn.perlmutter.nersc.gov>
2024-05-13 17:07:42 -0700

    new

--- simulations/LCHS/DiffConv/D001_t01_kmax10/job-script.sh
+++ simulations/LCHS/DiffConv/D001_t01_kmax10/job-script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -G 1
-#SBATCH -C gpu&hbm80g
+##SBATCH -C gpu&hbm80g
+#SBATCH -C gpu
 #SBATCH -n 1
 #SBATCH -c 32
-##SBATCH --gpus-per-node=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=novikau1@llnl.gov
 #SBATCH --account=mp2_g
 #SBATCH --qos=shared
-#SBATCH -t 00:05:00
+#SBATCH -t 01:00:00
 #SBATCH -J CDE_t01_kmax10
 #
 ## To run
