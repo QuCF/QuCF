8c9debdf8088d22138cc2bbf6072a1a23c0c4aff
QuCF <dauhalyhi@gmail.com>
2024-07-07 17:49:34 -0700

    BE-3

---
+++ simulations/LCHS/KvN/OPT3-kmax40/job-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C gpu&hbm80g
+##SBATCH -C gpu
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=novikau1@llnl.gov
+#SBATCH --account=mp2_g
+##SBATCH --qos=shared
+#SBATCH --qos=regular
+##SBATCH --mem=64G
+#SBATCH -t 10:00:00
+#SBATCH -J t1
+#
+## To run
+export SLURM_CPU_BIND="cores"
+srun /global/homes/n/novikau1/QuCF/QuCF/build_qucf/QuCF kvn ./
