c4d863361f35aa5a8412e45b45daa7248b9be3da
QuCF <dauhalyhi@gmail.com>
2024-10-02 17:14:37 -0700

    new

---
+++ simulations/LCHS/ADE-OPT/scan_t_kmax40/t2.0/job-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C gpu&hbm80g
+##SBATCH -C gpu
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=novikau1@llnl.gov
+#SBATCH --account=mp2_g
+
+##SBATCH --qos=debug
+##SBATCH --qos=shared
+#SBATCH --qos=regular
+
+##SBATCH --mem=64G
+
+#SBATCH -t 18:00:00
+#SBATCH -J k40_t20
+#
+## To run
+export SLURM_CPU_BIND="cores"
+srun /global/homes/n/novikau1/QuCF/QuCF/build_qucf/QuCF dc ./
